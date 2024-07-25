9d4faa315e948c72e69f45c4f1622fc0ea195a5b
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2024-07-25 11:32:48 -0300

    refactor: remove scaffold

--- src/domain/repository/TransactionRepository.ts
+++
@@ -1,6 +0,0 @@
-import Transaction from '../entity/Transaction';
-
-export default interface TransactionRepository {
-  save (transaction: Transaction): Promise<void>;
-  get (code: string): Promise<Transaction>;
-}
